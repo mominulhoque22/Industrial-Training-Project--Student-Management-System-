13b7dfc91f43d87c4398210e6074718216a218f4
Mominul Hoque <mominulhoqueshaklin693@gmail.com>
2026-07-19 19:46:17 +0600

    Delete sciencefaculty directory

--- sciencefaculty/validators.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-def validate_salary(value):
-    if value < 0:
-        raise serializers.ValidationError("salary must be a positive number.")
-    return value
